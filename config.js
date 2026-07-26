a582f71bc3c6aa172e3abddcb250a4499506efd1
muzamilhussain42643-rgb <muzamilhussain42643@gmail.com>
2026-07-26 05:33:48 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://fqveilqmnzsbhlviexla";
+
+const SUPABASE_KEY = "sb_publishable_981QCbTTCWmSQdbXewen2Q_4qx5sobD";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
